487acaa401342c02aef42888e3788d7508144021
Ginnyelf <ginnyelf0801@gmail.com>
2023-04-11 21:27:09 +0300

    Second commit

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -11,6 +11,7 @@ public class ManagerTest {
     private NameOfFilm film5 = new NameOfFilm("Человек-невидимка");
     private NameOfFilm film6 = new NameOfFilm("Тролли");
 
+    private NameOfFilm film7 = new NameOfFilm("Сверхъестественное");
 
     @Test
 
@@ -56,6 +57,7 @@ public class ManagerTest {
         film.add(film4);
         film.add(film5);
         film.add(film6);
+        film.add(film7);
 
 
         NameOfFilm[] expected = {film1, film2, film3, film4, film5, film6};
@@ -76,6 +78,18 @@ public class ManagerTest {
 
         NameOfFilm[] expected = {film1, film2, film3, film4, film5};
         NameOfFilm[] actual = film.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldLastSThreeMovie() {
+        film.add(film1);
+        film.add(film2);
+        film.add(film3);
+
+        NameOfFilm[] expected = {film3, film2, film1};
+        NameOfFilm[] actual = film.findLastFive();
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -106,8 +120,9 @@ public class ManagerTest {
         film.add(film4);
         film.add(film5);
         film.add(film6);
+        film.add(film7);
 
-        NameOfFilm[] expected = {film6, film5, film4, film3, film2};
+        NameOfFilm[] expected = {film7, film6, film5, film4, film3};
         NameOfFilm[] actual = film.findLastFive();
 
         Assertions.assertArrayEquals(expected, actual);
